bf990789f49c8ec9dd497a35ae07488978ebbd1f
wangtong <wangtong@buckyos.com>
2017-08-08 14:09:50 +0800

    完善编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var watch = require('gulp-watch');
 
 gulp.task("compile", function () {
     return tsProject.src()
@@ -21,4 +22,18 @@ gulp.task('res', () => {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', ['res', 'compile']);
+
+gulp.task('watch-res', () => {
+    return watch([
+            './src/**/*.png',
+            './src/**/*.jpg',
+            './src/**/*.gif',
+            './src/**/*.ico',
+            './src/**/*.wxml',
+            './src/**/*.wxss',
+            './src/**/*.json',
+        ])
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', ['watch-res']);
